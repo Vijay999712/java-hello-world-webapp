b1b4ad2a24b802dfb58c83ac8cdafe43ea5b8796
Vijay999712 <vijaybabu920@gmail.com>
2025-05-26 16:29:42 +0530

    Create HelloWorldServlet.java

---
+++ src/main/webapp/HelloWorldServlet.java
@@ -0,0 +1,18 @@
+package org.cloudifysource.examples;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "HelloWorldServlet", urlPatterns = {"/hello"})
+public class HelloWorldServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/plain");
+        resp.getWriter().write("Hello, World! This is your servlet running on Tomcat.");
+    }
+}
